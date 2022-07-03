5d24b61e0727b4a14c0f17da90c4c3d57c85bedf
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-03 17:56:38 -0400

    PC

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the Integer Guessing Game. Please enter your first guess: ");
+        if ()
     }
 }
